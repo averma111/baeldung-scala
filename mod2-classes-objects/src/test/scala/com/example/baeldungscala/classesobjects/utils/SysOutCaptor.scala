f26fb4f0c1797308b1d2b2b0b1d5e8718b3e3b71
micogongob <15572400+micogongob@users.noreply.github.com>
2021-08-14 20:18:23 +0800

    Add System.out captor utils class for test

---
+++ mod2-classes-objects/src/test/scala/com/example/baeldungscala/classesobjects/utils/SysOutCaptor.scala
@@ -0,0 +1,12 @@
+package com.example.baeldungscala.classesobjects.utils
+
+import java.io.{ByteArrayOutputStream, PrintStream}
+
+class SysOutCaptor {
+  val standardOut: PrintStream = System.out
+  val outStreamCaptor: ByteArrayOutputStream = new ByteArrayOutputStream()
+
+  def outputCaptor: PrintStream = new PrintStream(outStreamCaptor)
+  def capturedString: String = outStreamCaptor.toString.trim
+  def oldOutput: PrintStream = standardOut
+}
