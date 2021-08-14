34a6e2114c17ab5a6176cf3d94cb55671f76e53a
micogongob <15572400+micogongob@users.noreply.github.com>
2021-08-14 19:40:10 +0800

    Define classes, abstract class and Traits as similar to interfaces {} from Java

---
+++ mod1-scala-intro/src/test/scala/com/example/baeldungscala/intro/_4_ClassDefinitionsTest.scala
@@ -0,0 +1,73 @@
+package com.example.baeldungscala.intro
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+
+class _4_ClassDefinitionsTest {
+
+  @Test
+  def shouldCreateFromClassUsingNewKeyboard = {
+    val employee = new Employee("John", 1000)
+    employee.incrementSalary()
+
+    Assertions.assertEquals(1020, employee.salary)
+    Assertions.assertEquals("name=John, salary=1020", employee)
+  }
+
+  // Note: using val or var in Constructor means the variables will be public
+  class Employee(val name: String, var salary: Int, annualIncrement: Int = 20) {
+    // Note: methods are public by default unless specified to be private or protected
+    def incrementSalary(): Unit = {
+      salary += annualIncrement
+    }
+    override def toString(): String = s"name=$name, salary=$salary"
+  }
+
+  @Test
+  def shouldImplementSetDataStructure_usingAbstractClasses = {
+    val integerSet: IntegerSet = Range(1, 10).foldLeft(new EmptyIntegerSet(): IntegerSet) {
+      (x, y) => x add y
+    }
+
+    Assertions.assertFalse(integerSet contains 11)
+  }
+
+  abstract class IntegerSet {
+    def add(x: Int): IntegerSet
+    def contains(x: Int): Boolean
+  }
+
+  class EmptyIntegerSet extends IntegerSet {
+    override def add(x: Int): IntegerSet = new NoneEmptyIntegerSet(x, this)
+    override def contains(x: Int): Boolean = false
+  }
+
+  class NoneEmptyIntegerSet(
+                           val head: Int,
+                           val tail: IntegerSet
+                           ) extends IntegerSet {
+    override def add(x: Int): IntegerSet = {
+      if (this contains x) this
+      else new NoneEmptyIntegerSet(x, this)
+    }
+
+    override def contains(x: Int): Boolean = {
+      head == x || (tail contains x)
+    }
+  }
+
+  @Test
+  def shouldApplyTraitFunctionalityToClass_asMixin = {
+    val employee = new Employee("John", 1000, 50) with StringUpperCaser
+    employee.incrementSalary()
+
+    Assertions.assertEquals(1020, employee.salary)
+    Assertions.assertEquals("NAME=JOHN, SALARY=1050", employee)
+  }
+
+  // Note: Similar to interfaces in Java
+  trait StringUpperCaser {
+    override def toString = super.toString toUpperCase
+  }
+
+}
